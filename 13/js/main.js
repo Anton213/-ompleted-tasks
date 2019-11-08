105162b41485fd7d27f329e7cfa64815ef93d745
Anton <romantot24@gmail.com>
2019-11-08 23:52:13 +0300

    Это двенадцатый комит

--- 13/js/main.js
+++ 13/js/main.js
@@ -117,6 +117,7 @@ window.addEventListener('DOMContentLoaded', function () {
     };
 
     let form = document.querySelector('.main-form'),
+        idForm = document.getElementById('form'),
         input = document.getElementsByTagName('input'),
         statusMessage = document.createElement('div');
 
@@ -128,9 +129,62 @@ window.addEventListener('DOMContentLoaded', function () {
 
         let request = new XMLHttpRequest();
         request.open('POST', 'server.php');
-        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
         let formData = new FormData(form);
-        request.send(formData);
+
+        let obj = {};
+        formData.forEach(function (value, key) {
+            obj[key] = value;
+        });
+        let json = JSON.stringify(obj);
+        request.send(json);
+
+        request.addEventListener('readystatechange', function () {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if (request.readyState === 4 && request.readyState === 200) {
+                statusMessage.innerHTML = message.success;
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+
+                for (let i = 0; i < input.length; i++) {
+                    input[i].value = '';
+                }
+        });
+    });
+
+    idForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        idForm.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+        let formData = new FormData(idForm);
+
+        let obj = {};
+        formData.forEach(function (value, key) {
+            obj[key] = value;
+        });
+        let json = JSON.stringify(obj);
+        request.send(json);
+
+        request.addEventListener('readystatechange', function () {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if (request.readyState === 4 && request.readyState === 200) {
+                statusMessage.innerHTML = message.success;
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+
+            for (let i = 0; i < input.length; i++) {
+                input[i].value = '';
+            }
+        });
     });
 });
 
